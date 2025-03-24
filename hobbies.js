48c95778eff8f67a5404601ce908642bb9a7e318
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-03-24 16:23:18 +0100

    feat: complete ex 2

---
+++ hobbies.js
@@ -0,0 +1,7 @@
+function myHobbies(hobbyOne, hobbyTwo, hobbyThree) {
+    return [hobbyOne,hobbyTwo,hobbyThree];
+}
+
+console.log(myHobbies('football', 'music', 'videogames'))
+
+module.exports = myHobbies;
